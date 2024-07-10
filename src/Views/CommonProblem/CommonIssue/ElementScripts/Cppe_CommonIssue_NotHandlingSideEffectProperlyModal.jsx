cbc68b740dc89e1b70dd7d603b4d442936be36c2
Huy Le <patrotothenus@gmail.com>
2024-07-10 08:50:47 +0700

    fixing issues of NotHandlingSideEffectsProperly

--- src/Views/CommonProblem/CommonIssue/ElementScripts/Cppe_CommonIssue_NotHandlingSideEffectProperlyModal.jsx
+++ src/Views/CommonProblem/CommonIssue/ElementScripts/Cppe_CommonIssue_NotHandlingSideEffectProperlyModal.jsx
@@ -1,35 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button, ListGroup, Modal } from "react-bootstrap";
 import { fetchTodo } from "UtilityScripts/Cppu_Common";
 
-
 //This file has 2 issue
 //1.Improper Handling of Effect
-//2.Not using keys in List 
+//2.Not using keys in List
 export const NotHandlingSideEffectsProperlyModal = ({ HideModal }) => {
-   const [arrData, SetData] = useState([]);
-   
-   fetchTodo(SetData);
-   return (
-      <Modal show={true} onHide={HideModal} centered>
-         <Modal.Header closeButton>
-            <Modal.Title>Not Handling Side Effect Properly</Modal.Title>
-         </Modal.Header>
-         <Modal.Body>
-            <ListGroup>
-               {arrData.map((item) => (
-                  <ListGroup.Item>
-                     <b>{item.email}</b>
-                     <div>{item.body}</div>
-                  </ListGroup.Item>
-               ))}
-            </ListGroup>
-         </Modal.Body>
-         <Modal.Footer>
-            <Button variant="secondary" onClick={HideModal}>
-               Close
-            </Button>
-         </Modal.Footer>
-      </Modal>
-   )
-}
+  const [arrData, SetData] = useState([]);
+
+  useEffect(() => {
+    function fetchingData() {
+      fetchTodo(SetData);
+    }
+    fetchingData();
+  }, [HideModal]);
+
+  return (
+    <Modal show={true} onHide={HideModal} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Not Handling Side Effect Properly</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <ListGroup>
+          {arrData.map((item) => (
+            <ListGroup.Item key={item.id}>
+              <b>{item.email}</b>
+              <div>{item.body}</div>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={HideModal}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
